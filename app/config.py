3b13da7c08af5aa8429c8ab695390cd0b3f8dfeb
Ruuuubje <mail@ruben.be>
2025-12-01 12:33:48 +0100

    IPV4 session pooler

--- app/config.py
+++ app/config.py
@@ -1,6 +1,11 @@
 class Config:
     SECRET_KEY = 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Barter.com123%21@db.flijuodhzqnqvabpgqpj.supabase.co:5432/postgres'
+    
+    SQLALCHEMY_DATABASE_URI = (
+        'postgresql://postgres.flijuodhzqnqvabpgqpj:Barter.com123%21'
+        '@aws-1-us-east-1.pooler.supabase.com:5432/postgres'
+    )
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     #de %21 is voor het ! teken in de wachtwoord
